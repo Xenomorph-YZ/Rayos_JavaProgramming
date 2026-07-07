9451f4c785737f42c9feef2545cf0c25bb2f1839
Xenomorph-YZ <jaceallenrayos@gmail.com>
2026-07-07 16:56:32 +0800

    Incomplete pa ito

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+       System.out.println("▐▀▀▀▀▀▀▀▀▀▀▌\r\n" + //
+                      "▐ PWU JASMS▌\r\n" + //
+                      "▐▄▄▄▄▄▄▄▄▄▄▌");
+System.out.println("............");
+
+    System.out.println("Welcome Students!!! <3");
+    System.out.println("Learn & Innovate Later!");
+    System.out.println("Sir Lan Sevilleno as our Teacher");
+    System.out.println("▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌\r\n" + //
+                "▐     Do Things you can be proud of      ▌\r\n" + //
+                "▐        so that you'll look back        ▌\r\n" + //
+                "▐  and be amazed of how far you've come  ▌\r\n" + //
+                "▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌");
+    }
+}
