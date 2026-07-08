69dfbdb48dbbe7a70ef94d0756a14fda9af36a1c
Xenomorph-YZ <jaceallenrayos@gmail.com>
2026-07-08 16:18:06 +0800

    JAVA Performance Task#1

--- Main.java
+++ Main.java
@@ -1,17 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-       System.out.println("▐▀▀▀▀▀▀▀▀▀▀▌\r\n" + //
-                      "▐ PWU JASMS▌\r\n" + //
-                      "▐▄▄▄▄▄▄▄▄▄▄▌");
-System.out.println("............");
-
+       System.out.println("______ _    _ _   _     ___  ___   ________  ___ _____ \r\n" + //
+                      "| ___ \\ |  | | | | |   |_  |/ _ \\ /  ___|  \\/  |/  ___|\r\n" + //
+                      "| |_/ / |  | | | | |     | / /_\\ \\\\ `--.| .  . |\\ `--. \r\n" + //
+                      "|  __/| |/\\| | | | |     | |  _  | `--. \\ |\\/| | `--. \\\r\n" + //
+                      "| |   \\  /\\  / |_| | /\\__/ / | | |/\\__/ / |  | |/\\__/ /\r\n" + //
+                      "\\_|    \\/  \\/ \\___/  \\____/\\_| |_/\\____/\\_|  |_/\\____/ \r\n" + //
+                      "                                                       \r\n" + //
+                      "                                                       ");
+//ok na yan kahit dalawang space from Welcome at PWU JASMS 
     System.out.println("Welcome Students!!! <3");
-    System.out.println("Learn & Innovate Later!");
-    System.out.println("Sir Lan Sevilleno as our Teacher");
+    System.out.println("\n Learn & Innovate Later!");
+    System.out.println("\n Sir Lan Sevilleno as our Teacher!!!");
     System.out.println("▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌\r\n" + //
                 "▐     Do Things you can be proud of      ▌\r\n" + //
                 "▐        so that you'll look back        ▌\r\n" + //
                 "▐  and be amazed of how far you've come  ▌\r\n" + //
                 "▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌");
+                System.out.println("I'm Jace Allen D. Rayos");
+                System.out.println("Grade 12-Aquinas, ICT Strand");
+                System.out.println("ICT is somewhere you can express your freedom through coding"); //I can also enhance my creativity here as well
+                System.out.println("Being able to express each emotion thoughout a set of text and run it, is one of the reasons why I chose ICT");
+                System.out.println("My dream job is to be Game Developer");
+
+                System.out.println("\n ───────────────────────────Motivational Quote──────────────────────────────");
+                System.out.println("The things that change us most are often the things that can't last forever");
+                System.out.println("───────────────────────────Motivational Quote──────────────────────────────");
+                System.out.println("\n  _____    _____    _____    _____    _____    _____    _____    _____    _____   \r\n" + //
+                                        " / ___ `. / ___ `. / ___ `. / ___ `. / ___ `. / ___ `. / ___ `. / ___ `. / ___ `. \r\n" + //
+                                        "|_/___) ||_/___) ||_/___) ||_/___) ||_/___) ||_/___) ||_/___) ||_/___) ||_/___) | \r\n" + //
+                                        "  /  __.'  /  __.'  /  __.'  /  __.'  /  __.'  /  __.'  /  __.'  /  __.'  /  __.' \r\n" + //
+                                        "  |_|      |_|      |_|      |_|      |_|      |_|      |_|      |_|      |_|     \r\n" + //
+                                        "  (_)      (_)      (_)      (_)      (_)      (_)      (_)      (_)      (_)     \r\n" + //
+                                        "                                                                                  ");
+                                        /*I chose ??? as an Art that represents myself since I don't
+                                        even understand my own nature as a person, I act the way I am
+                                        based on the people around me*/
+                                        //End! <3
     }
 }
