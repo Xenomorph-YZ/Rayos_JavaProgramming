f783c2a3ed56c1a6c033f4d52713f6a2199c3d61
Xenomorph-YZ <jaceallenrayos@gmail.com>
2026-07-15 00:04:40 +0800

    Last Saturday Activity na ngayon lang na commit

---
+++ Lesson6Activity1.java
@@ -0,0 +1,33 @@
+public class Lesson6Activity1 {
+    public static void main(String[] args) {
+
+        String firstName = "Jace Allen ";
+        char middleName = 'D';
+        String lastName = "Rayos";
+        String fullName = firstName + middleName + ". " + lastName;
+        String nickname = "Jace";
+        System.out.println("Name: " + fullName);
+        System.out.println("Nickname : " + nickname);
+
+        int age = 16;
+        System.out.println("Age: " + age);
+
+        int gradeLevel = 12;
+        System.out.println("Grade Level: " + gradeLevel);
+
+        String section = "Aquinas";
+         System.out.println("Section: " + section);
+
+        String school = "PWU JASMS QC";
+         System.out.println("School: " + school);
+
+        String favoriteSubject = "Recess/Dismissal";
+        System.out.println("Favorite Subject: " + favoriteSubject);
+
+        float averageGrade = 88.9f;
+        System.out.println("Average Grade: " + averageGrade);
+
+        boolean honorStudent = false;
+        System.out.println("Honor Student: " + honorStudent);
+    }
+}
