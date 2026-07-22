c742c0b721eae454f6c99ddc13ab8be616f4db91
Xenomorph-YZ <jaceallenrayos@gmail.com>
2026-07-22 15:52:08 +0800

    :P

--- Activity7.java
+++ Activity7.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
+
 public class Activity7 {
     public static void main(String[] args) {
-
-    Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter Email:");
-        String email = scanner.nextLine();
+        
+        Scanner scn = new Scanner(System.in);
+        System.out.println("ENTER EMAIL: ");
+        String email = scn.nextLine();
+        
         int atSymbol = email.indexOf("@");
-        String username = email.substring(0, atSymbol);
-        String domain = email.substring(atSymbol);
-        System.out.println("\n");
-        System.out.println("Email Entered:\n" + email);
-        System.out.println("Length: " + email.length());
-        System.out.println("Username: " + username);
-        System.out.println("Domain: " + domain);
+
+        String username = email.substring(0,atSymbol);
+        System.out.println(username);
+
+        String domain = email.substring(atSymbol +1);
+        System.out.println(domain);
+
     }
 }
