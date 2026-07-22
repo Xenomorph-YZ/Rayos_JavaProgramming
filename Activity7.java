6b1ccab51216b41d1d5dd09bb0af4ca71fab96d7
Xenomorph-YZ <jaceallenrayos@gmail.com>
2026-07-22 15:50:52 +0800

    Update Activity7.java

--- Activity7.java
+++ Activity7.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 public class Activity7 {
     public static void main(String[] args) {
 
-    //String email = "jaceallenrayos@gmail.com";
-    Scanner email = new Scanner(System.in);
-    System.out.println("ENTER YOUR EMAIL:");
-    String userEmail = email.nextLine();
-
-    System.out.println("The length of the text string is: " + userEmail.length());
-    String userName = userEmail;
-    System.out.println(userName.toLowerCase());
-    
-    System.out.println(userName.indexOf(""));
-    System.out.println(userName.substring(0,4)); 
+    Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Email:");
+        String email = scanner.nextLine();
+        int atSymbol = email.indexOf("@");
+        String username = email.substring(0, atSymbol);
+        String domain = email.substring(atSymbol);
+        System.out.println("\n");
+        System.out.println("Email Entered:\n" + email);
+        System.out.println("Length: " + email.length());
+        System.out.println("Username: " + username);
+        System.out.println("Domain: " + domain);
     }
 }
